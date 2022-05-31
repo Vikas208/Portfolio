d7c33a39c571ea38f632581fbdce5789b75d7d40
vikas <technologyguide44@gmail.com>
2022-05-31 21:38:22 +0530

    Fix Issues

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -12,10 +12,10 @@ mongoConnect();
 app.use("/user", userRouter)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.resolve("../frontend/build")));
+app.use(express.static(path.resolve("./frontend/build")));
 
 app.use("*", (req, res) => {
-       res.sendFile(path.resolve("../frontend/build/index.html"));
+       res.sendFile(path.resolve("./frontend/build/index.html"));
 })
 
 app.listen(process.env.PORT, () => {
